e052af98649289d12145c08483775a9745cca6a7
Jeevan501 <jeevana591501@gmail.com>
2023-11-16 09:27:57 +0530

    peakelementarray

---
+++ nearlysorted.js
@@ -0,0 +1,18 @@
+function sorted(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] > target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return -1;
+}
+arr = [2, 5, 8, 26, 29, 88];
+target = 26;
+console.log(sorted(arr, target));
